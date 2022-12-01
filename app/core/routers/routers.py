a1471c05ee9023d3207759484655d1146dc7f7ca
markushik <zem_mark@yahoo.com>
2022-12-01 19:15:08 +0300

    minimal example usage fms

--- app/core/routers/routers.py
+++ app/core/routers/routers.py
@@ -1,5 +1,22 @@
-from aiogram import types
+"""
+The file responsible for use commands in bot
+"""
+import os
+
+from aiogram import Router
+from aiogram.filters import Command
+from aiogram.types import Message
+
+router = Router()
 
 
-async def start(message: types.Message) -> None:
-    await message.answer("hello")
+@router.message(Command(commands="start"))
+async def send_pong(message: Message) -> None:
+    pong = ""
+    hostname = '127.0.0.1'
+    response = os.system('ping -c 5 ' + hostname)
+    if response == 0:
+        pong = (' is up!')
+    else:
+        pong = (' is down!')
+    await message.reply(pong)
